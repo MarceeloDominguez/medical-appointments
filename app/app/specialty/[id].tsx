d79cd794c6b5e5fb2e161a922002913d82ae8d98
marcelo dominguez <marceelodominguez@gmail.com>
2025-07-14 16:00:13 -0300

    add doctor card ui

--- app/app/specialty/[id].tsx
+++ app/app/specialty/[id].tsx
@@ -1,8 +1,16 @@
+import CustomSearch from "@/components/CustomSearch";
+import DoctorCard from "@/components/DoctorCard";
 import { Colors } from "@/constants/Colors";
 import { useSpecialtyById } from "@/queries/specialties";
 import { useLocalSearchParams, useNavigation } from "expo-router";
 import React, { useEffect } from "react";
-import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function SpecialtyDetails() {
   const { id } = useLocalSearchParams();
@@ -21,17 +29,34 @@ export default function SpecialtyDetails() {
           <Text style={styles.specialtyName}>{data.name}</Text>
         ),
       headerShadowVisible: false,
+      headerStyle: {
+        backgroundColor: Colors.light.background,
+      },
     });
   }, [navigation, isLoading, data]);
 
   return (
-    <View>
-      <Text>SpecialtyDetails</Text>
+    <View style={styles.container}>
+      <CustomSearch />
+      <FlatList
+        keyExtractor={(_, index) => index.toString()}
+        data={[1, 2, 3, 4, 5]}
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponentStyle={{ marginBottom: 10 }}
+        contentContainerStyle={styles.contentContainerStyle}
+        renderItem={({ item }) => <DoctorCard />}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.light.background,
+    paddingHorizontal: 20,
+  },
+  contentContainerStyle: { gap: 20, paddingBottom: 60, paddingTop: 20 },
   specialtyName: {
     color: Colors.light.text,
     fontWeight: "600",
---
+++ app/components/DoctorCard.tsx
@@ -0,0 +1,63 @@
+import { Colors } from "@/constants/Colors";
+import { default as IconStar } from "@expo/vector-icons/Ionicons";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function DoctorCard() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.avatar}></View>
+      <View style={{ flex: 1 }}>
+        <Text numberOfLines={1} style={styles.doctorName}>
+          Dr. Stone Gaze
+        </Text>
+        <Text numberOfLines={1} style={styles.doctorBio}>
+          Cardiologa
+        </Text>
+      </View>
+      <View style={styles.wrapperIconStar}>
+        <IconStar name="star-sharp" size={16} color="#f38744" />
+        <Text style={styles.rating}>4.5</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    gap: 10,
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 4,
+    flex: 1,
+  },
+  avatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: "#a6a8aa",
+  },
+  doctorName: {
+    fontSize: 15,
+    fontWeight: "bold",
+    color: Colors.light.text,
+  },
+  doctorBio: {
+    fontSize: 13,
+    color: Colors.light.textSecondary,
+    marginTop: 4,
+  },
+  wrapperIconStar: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 5,
+    padding: 6,
+  },
+  rating: {
+    color: Colors.light.textSecondary,
+    fontWeight: "600",
+    fontSize: 13,
+  },
+});
